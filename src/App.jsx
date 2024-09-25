cbe5bd60a8ee8574675fff785738c50a2d4da2d9
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-26 01:42:08 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from './components/Layout';
+import Loader from './components/Loader/Loader';
+
+import 'modern-normalize';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const CatalogPage = lazy(() => import('./pages/CatalogPage/CatalogPage'));
+
 function App() {
-  return <></>;
+  return (
+    <Layout>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+        </Routes>
+      </Suspense>
+    </Layout>
+  );
 }
 
 export default App;
