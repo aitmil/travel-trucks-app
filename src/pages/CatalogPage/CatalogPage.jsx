d4f20bcb76e4ddb5047702b28dca95c10ff84b64
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-26 22:54:11 +0300

    update Catalog page

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,5 +1,19 @@
+import Loader from '../../components/Loader/Loader';
+import TruckList from '../../components/TruckList/TruckList';
+
 import css from './CatalogPage.module.css';
+import Filter from '../../components/Filter/Filter';
+import { useSelector } from 'react-redux';
+import { selectLoading } from '../../redux/trucks/selectors';
 
 export default function CatalogPage() {
-  <></>;
+  const isLoading = useSelector(selectLoading);
+
+  return (
+    <main className={css.container}>
+      <Filter />
+      {isLoading && <Loader />}
+      <TruckList />
+    </main>
+  );
 }
