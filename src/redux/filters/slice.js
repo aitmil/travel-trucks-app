d8f1fd6b5fbd89b2209fc8f63c46c6e1a9101720
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-26 22:53:32 +0300

    add filters redux

---
+++ src/redux/filters/slice.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  location: '',
+  truckEquipment: {
+    automatic: false,
+    kitchen: false,
+    ac: false,
+    tv: false,
+    bathroom: false,
+  },
+  truckType: {
+    van: false,
+    fullyIntegrated: false,
+    alcove: false,
+  },
+};
+
+const filterSlice = createSlice({
+  name: 'filters',
+  initialState,
+  reducers: {
+    setLocation(state, action) {
+      state.location = action.payload;
+    },
+    toggleTruckEquipment(state, action) {
+      const key = action.payload;
+      state.truckEquipment[key] = !state.truckEquipment[key];
+    },
+    setTruckType(state, action) {
+      const selectedType = action.payload;
+      Object.keys(state.truckType).forEach(type => {
+        state.truckType[type] = type === selectedType;
+      });
+    },
+  },
+});
+
+export const { setLocation, toggleTruckEquipment, setTruckType } =
+  filterSlice.actions;
+export const filtersReducer = filterSlice.reducer;
