2522b4eda332401cd59e79304bcce564462550bc
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-27 22:15:50 +0100

    add favorites redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { filtersReducer } from './filters/slice';
 import { trucksReducer } from './trucks/slice';
+import { favoritesReducer } from './favourites/slice';
 
-const store = configureStore({
+const persistConfig = {
+  key: 'favorites',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, favoritesReducer);
+
+export const store = configureStore({
   reducer: {
     filters: filtersReducer,
     trucks: trucksReducer,
+    favorites: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export default store;
+export const persistor = persistStore(store);
