a6df2e81bb0e730e264b5678cce29ea1c83d76a7
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-27 22:17:30 +0100

    add Error rendering

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,19 +1,30 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import Loader from '../../components/Loader/Loader';
+import Error from '../../components/Error/Error';
 import TruckList from '../../components/TruckList/TruckList';
+import Filter from '../../components/Filter/Filter';
+
+import { selectLoading, selectError } from '../../redux/trucks/selectors';
+import { fetchTrucks } from '../../redux/trucks/operations';
 
 import css from './CatalogPage.module.css';
-import Filter from '../../components/Filter/Filter';
-import { useSelector } from 'react-redux';
-import { selectLoading } from '../../redux/trucks/selectors';
 
 export default function CatalogPage() {
+  const dispatch = useDispatch();
   const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchTrucks());
+  }, [dispatch]);
 
   return (
     <main className={css.container}>
       <Filter />
       {isLoading && <Loader />}
-      <TruckList />
+      {isError ? <Error /> : <TruckList />}
     </main>
   );
 }
