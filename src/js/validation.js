9e5e5389cdc3f7f5da7bae459827e833149df919
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-29 02:09:21 +0300

    fix validation

--- src/js/validation.js
+++ src/js/validation.js
@@ -3,8 +3,6 @@ import * as Yup from 'yup';
 export const BookingSchema = Yup.object().shape({
   name: Yup.string().required('Name is required').min(2, 'Name is too short'),
   email: Yup.string().required('Email is required').email('Invalid email'),
-  date: Yup.date()
-    .required('Booking date is required')
-    .min(new Date(), 'Booking date cannot be in the past'),
+  date: Yup.date().required('Booking date is required').nullable(),
   comment: Yup.string().max(500, 'Comment is too long'),
 });
