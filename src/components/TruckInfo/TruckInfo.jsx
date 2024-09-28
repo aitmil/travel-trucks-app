6de00a9c7dcd9b4a0cca13323f1c491703a0d877
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-28 17:11:33 +0300

    add Truck info component

---
+++ src/components/TruckInfo/TruckInfo.jsx
@@ -0,0 +1,48 @@
+import { formatLocation, formatPrice } from '../../js/utils';
+import icons from '../../assets/icons.svg';
+import css from './TruckInfo.module.css';
+
+export default function TruckInfo({
+  truck: { name, price, rating, reviews, description, gallery, location },
+}) {
+  return (
+    <>
+      <section className={css.section}>
+        <h3 className={css.title}>{name}</h3>
+
+        <div className={css.reviewBox}>
+          <div className={css.review}>
+            <svg className={css.star} width={16} height={16}>
+              <use xlinkHref={`${icons}#icon-star`} />
+            </svg>
+            <p
+              className={css.reviewText}
+            >{`${rating}(${reviews.length} Reviews)`}</p>
+          </div>
+
+          <div className={css.location}>
+            <svg className={css.star} width={16} height={16}>
+              <use xlinkHref={`${icons}#icon-map`} />
+            </svg>
+            <p>{formatLocation(location)}</p>
+          </div>
+        </div>
+
+        <h3 className={css.title}>{formatPrice(price)}</h3>
+
+        <ul className={css.list}>
+          {gallery.map((image, index) => (
+            <li className={css.item} key={index}>
+              <img
+                className={css.img}
+                src={image.thumb}
+                alt="photo of truck"
+              ></img>
+            </li>
+          ))}
+        </ul>
+        <p className={css.text}>{description}</p>
+      </section>
+    </>
+  );
+}
