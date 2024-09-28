6ab59b85ee6d79e0c9f165af08ff2bca28ba7cba
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-28 17:10:39 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 
 import Layout from './components/Layout';
 import Loader from './components/Loader/Loader';
+import TruckFeatures from './components/TruckFeatures/TruckFeatures';
+import TruckReviews from './components/TruckReviews/TruckReviews';
 
 import 'modern-normalize';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const CatalogPage = lazy(() => import('./pages/CatalogPage/CatalogPage'));
+const TruckDetailsPage = lazy(() =>
+  import('./pages/TruckDetailsPage/TruckDetailsPage')
+);
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
 function App() {
   return (
@@ -16,6 +22,11 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/catalog/:id" element={<TruckDetailsPage />}>
+            <Route path="features" element={<TruckFeatures />} />
+            <Route path="reviews" element={<TruckReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
     </Layout>
