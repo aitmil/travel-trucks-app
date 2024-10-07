0dad0000cbd0765b2288dfba63d2fe208940561a
Anna Taranchuk <taranchuk4@yahoo.com>
2024-10-07 23:51:09 +0300

    add filter params

--- src/redux/trucks/operations.js
+++ src/redux/trucks/operations.js
@@ -5,10 +5,37 @@ axios.defaults.baseURL = 'https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/';
 
 export const fetchTrucks = createAsyncThunk(
   'trucks/fetchAll',
-  async (page = 1, thunkAPI) => {
+  async ({ page, filters = {}, reset = false }, thunkAPI) => {
     try {
-      const response = await axios.get(`/campers?page=${page}&limit=4`);
-      return response.data;
+      const params = new URLSearchParams({
+        page: page.toString(),
+        limit: '4',
+      });
+
+      if (filters) {
+        Object.entries(filters.truckEquipment).forEach(
+          ([equipmentKey, value]) => {
+            if (value) {
+              params.append(equipmentKey, 'true');
+            }
+          }
+        );
+
+        if (filters.form) {
+          params.append('form', filters.form);
+        }
+
+        if (filters.location) {
+          params.append('location', filters.location);
+        }
+
+        if (filters.transmission) {
+          params.append('transmission', filters.transmission);
+        }
+      }
+
+      const { data } = await axios.get(`/campers?${params.toString()}`);
+      return { reset, ...data };
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
