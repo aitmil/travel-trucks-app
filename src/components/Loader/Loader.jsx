cdc19730d5ed97df3fd5d2011cb55d9c329798b1
Anna Taranchuk <taranchuk4@yahoo.com>
2024-10-09 01:07:38 +0300

    fix loader styles

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,16 +3,18 @@ import css from './Loader.module.css';
 
 export default function Loader() {
   return (
-    <>
-      <ThreeDots
-        visible={true}
-        height="100"
-        width="100"
-        color="#d84343"
-        radius="9"
-        ariaLabel="three-dots-loading"
-        wrapperClass={css.loader}
-      />
-    </>
+    <div className={css.backdrop}>
+      <div className={css['loader-group']}>
+        <ThreeDots
+          visible={true}
+          height="100"
+          width="100"
+          color="#d84343"
+          radius="9"
+          ariaLabel="three-dots-loading"
+          wrapperClass={css.loader}
+        />
+      </div>
+    </div>
   );
 }
