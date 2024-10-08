06853fdf1da6b0db98f0893ede36bf108097c8a0
Anna Taranchuk <taranchuk4@yahoo.com>
2024-10-09 01:05:12 +0300

    add error when items not found

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,17 +1,25 @@
+import { useSelector } from 'react-redux';
+import { selectError } from '../../redux/trucks/selectors';
 import { GiTerror } from 'react-icons/gi';
 import { IoReload } from 'react-icons/io5';
 import css from './Error.module.css';
 
 export default function Error() {
-  return (
-    <div className={css.errorBox}>
-      <GiTerror size={48} className={css.iconMain} />
-      <div className={css.error}>
-        <p className={css.text}>
-          Ooops... Something went wrong! Try to reload this page
-        </p>
-        <IoReload />
+  const error = useSelector(selectError);
+
+  if (error === 'Items matching your filter not found') {
+    return <p className={css.error}>{`${error}!`}</p>;
+  } else {
+    return (
+      <div className={css.errorBox}>
+        <GiTerror size={48} className={css.iconMain} />
+        <div className={css.error}>
+          <p className={css.text}>
+            Ooops... Something went wrong! Try to reload this page
+          </p>
+          <IoReload />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
