fba1419ad3a6ab914670b940e62f7efe88696c56
Anna Taranchuk <taranchuk4@yahoo.com>
2024-10-09 23:58:34 +0300

    add DatePicker component

---
+++ src/components/DatePicker/DatePicker.jsx
@@ -0,0 +1,68 @@
+import { useState, forwardRef } from 'react';
+import DatePicker from 'react-datepicker';
+import { registerLocale } from 'react-datepicker';
+import { enUS } from 'date-fns/locale';
+import { format, isToday } from 'date-fns';
+
+import FormField from '../FormField/FormField';
+import { saveToSessionStorage } from '../../js/sessionStorage';
+
+import 'react-datepicker/dist/react-datepicker.css';
+import './DatePicker.css';
+
+const Datepicker = ({ onChange, selected }) => {
+  const [placeholder, setPlaceholder] = useState('Booking date*');
+
+  const CustomInput = forwardRef(({ value, onClick }, ref) => (
+    <FormField
+      name="bookingDate"
+      value={value}
+      onClick={onClick}
+      onFocus={e => {
+        setPlaceholder('Select a date after today');
+        onClick(e);
+      }}
+      onBlur={() => {
+        if (!value) setPlaceholder('Booking date*');
+      }}
+      placeholder={placeholder}
+      ref={ref}
+    />
+  ));
+
+  CustomInput.displayName = 'CustomInput';
+
+  const customLocale = {
+    ...enUS,
+    options: {
+      ...enUS.options,
+      weekStartsOn: 1,
+    },
+    localize: {
+      ...enUS.localize,
+      day: n => format(new Date(2023, 0, n + 1), 'EEE'),
+    },
+  };
+
+  registerLocale('custom-en', customLocale);
+
+  const customDayClassName = date => {
+    return isToday(date) ? 'today' : undefined;
+  };
+
+  return (
+    <DatePicker
+      locale="custom-en"
+      selected={selected}
+      onChange={date => {
+        onChange(date);
+        saveToSessionStorage('bookingDate', date);
+      }}
+      dateFormat="yyyy/MM/dd"
+      customInput={<CustomInput />}
+      dayClassName={customDayClassName}
+    />
+  );
+};
+
+export default Datepicker;
