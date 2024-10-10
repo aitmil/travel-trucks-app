fefca9223f9c308538f230a3e2a6b33a1ce38403
Anna Taranchuk <taranchuk4@yahoo.com>
2024-10-10 22:12:17 +0300

    add scroll to pagination

--- src/js/sessionStorage.js
+++
@@ -1,8 +0,0 @@
-export const saveToSessionStorage = (name, value) => {
-  const data = value instanceof Date ? value.toISOString() : value;
-  sessionStorage.setItem(name, data);
-};
-
-export const getItemFromSessionStorage = name => {
-  return sessionStorage.getItem(name);
-};
