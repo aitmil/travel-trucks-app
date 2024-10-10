486456e52e5043549d971f1b04d807f3e12ff4ae
Anna Taranchuk <taranchuk4@yahoo.com>
2024-10-10 22:13:08 +0300

    transfer pagination with LoadMore to separate component

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,42 +1,32 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import Loader from '../../components/Loader/Loader';
 import Error from '../../components/Error/Error';
 import TruckList from '../../components/TruckList/TruckList';
 import Filter from '../../components/Filter/Filter';
+import LoadMoreBtn from '../../components/LoadMoreBtn/LoadMoreBtn';
 
 import {
   selectLoading,
   selectError,
-  selectTotal,
-  selectFilteredTrucks,
+  selectTrucks,
 } from '../../redux/trucks/selectors';
+import { selectFilters } from '../../redux/filters/selectors';
 import { fetchTrucks } from '../../redux/trucks/operations';
 
 import css from './CatalogPage.module.css';
 
 export default function CatalogPage() {
-  const dispatch = useDispatch();
   const isLoading = useSelector(selectLoading);
   const isError = useSelector(selectError);
-  const total = useSelector(selectTotal);
-  const filteredTrucks = useSelector(selectFilteredTrucks);
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [showBtn, setShowBtn] = useState(false);
-  const perPage = 4;
+  const trucks = useSelector(selectTrucks);
+  const filters = useSelector(selectFilters);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchTrucks(currentPage));
-    setShowBtn(Math.round(total / perPage) !== currentPage);
-  }, [dispatch, currentPage, total]);
-
-  const handleLoadMore = () => {
-    if (filteredTrucks.length < total) {
-      setCurrentPage(prev => prev + 1);
-    }
-  };
+    dispatch(fetchTrucks({ page: 1, filters, reset: true }));
+  }, [dispatch, filters]);
 
   return (
     <main className={css.container}>
@@ -44,12 +34,8 @@ export default function CatalogPage() {
       {isLoading && <Loader />}
       {isError && <Error />}
       <div className={css.wrapper}>
-        {filteredTrucks.length > 0 && <TruckList />}
-        {showBtn && filteredTrucks.length > 0 && !isLoading && (
-          <button onClick={handleLoadMore} className={css.loadMore}>
-            Load More
-          </button>
-        )}
+        {trucks.length > 0 && <TruckList />}
+        <LoadMoreBtn />
       </div>
     </main>
   );
